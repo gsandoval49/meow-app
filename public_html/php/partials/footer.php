b1855a9b1d28e7b15308868c28e8c374873a4191
Giles Sandoval <gsandoval2277@gmail.com>
2016-08-24 08:48:20 -0600

    require once added

---
+++ public_html/php/partials/footer.php
@@ -0,0 +1,10 @@
+<!--begin footer -->
+<footer>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xs-12">
+                &copy;&nbsp;2016 Romulan Star Empire
+            </div>
+        </div>
+    </div>
+</footer>
